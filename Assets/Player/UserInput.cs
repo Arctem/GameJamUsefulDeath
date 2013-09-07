a5bc6182558fd4105f674656b2f41ac11fe3606d
Russell White <rarctem@gmail.com>
2013-09-06 20:18:11 -0700

    Added FPS controls

--- Assets/Player/UserInput.cs
+++ Assets/Player/UserInput.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using FPS;
 
 public class UserInput : MonoBehaviour {
+	private Player player;
 
 	// Use this for initialization
 	void Start () {
-	
+		player = transform.root.GetComponent<Player>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+	}
 	
+	private void MoveCamera() {
+		
+	}
+	
+	private void RotateCamera() {
+		
 	}
 }
---
+++ Assets/ResourceManager/ResourceManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FPS {
+	public class ResourceManager {
+		public static int MoveSpeed { get { return 25; } }
+		public static int RotateSpeed { get { return 100; } }
+	}	
+}
+
