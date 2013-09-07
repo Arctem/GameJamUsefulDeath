ce6c06fa711ec55eaa7db46df01bd7a6a4d9bdd3
Russell White <rarctem@gmail.com>
2013-09-06 20:39:57 -0700

    Added simple enemy that moves forward.

---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	
+	CharacterController controller;
+
+	// Use this for initialization
+	void Start() {
+		controller = transform.root.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		controller.SimpleMove(new Vector3(3, 0, 0));
+	}
+}
